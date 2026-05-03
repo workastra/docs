3b09395954d7f0fbdf06db4e44347ebe0b044fca
Phong Chu <chucamphong1999@gmail.com>
2026-05-03 21:03:06 +0700

    fix: correct GitHub URL path in ViewOptionsPopover component

--- src/app/shared/[[...slug]]/page.tsx
+++ src/app/shared/[[...slug]]/page.tsx
@@ -31,7 +31,7 @@ export default async function Page(props: PageProps<"/shared/[[...slug]]">) {
         <MarkdownCopyButton markdownUrl={markdownUrl} />
         <ViewOptionsPopover
           markdownUrl={markdownUrl}
-          githubUrl={`https://github.com/${gitConfig.user}/${gitConfig.repo}/blob/${gitConfig.branch}/content/docs/${page.path}`}
+          githubUrl={`https://github.com/${gitConfig.user}/${gitConfig.repo}/blob/${gitConfig.branch}/content/${page.path}`}
         />
       </div>
       <DocsBody>
