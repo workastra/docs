07a63aab2f5bdfd8075ad2ed89827034e032f2c3
Phong Chu <chucamphong1999@gmail.com>
2026-05-03 19:59:56 +0700

    feat: add GitHub Actions workflow for deploying Next.js site and update documentation structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,9 @@ export default function Layout({ children }: LayoutProps<'/'>) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
-        <RootProvider>{children}</RootProvider>
+        <RootProvider search={{
+          enabled: false
+        }}>{children}</RootProvider>
       </body>
     </html>
   );
